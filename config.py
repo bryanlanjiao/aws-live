97e6b4440558b7f51482d53152ca4ea61e224ca8
bryanlanjiao <167164913+bryanlanjiao@users.noreply.github.com>
2024-04-16 13:54:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.c9ii0gqs4149.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "lowkimhong-employee"
+customregion = "us-east-1"
+
